a678fd5835a266231a0c05abc1067c8e26ee0f3c
Manthan harale <126886816+manthan20021@users.noreply.github.com>
2025-10-04 08:23:20 -0700

    changd the text

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1 className="bg-blue-400 text-pink-600">pahe</h1>
+      <h1 className="bg-blue-400 text-pink-600">rugved umak..</h1>
     </div>
   );
 }
