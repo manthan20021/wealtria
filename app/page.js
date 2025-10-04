a5d7e8b839498a28fb4fb97ccbe9a21709add7b8
Manthan harale <126886816+manthan20021@users.noreply.github.com>
2025-10-04 08:13:36 -0700

    removd the brekpoints in tailwind css

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1 className="bg-amber-400 md:bg-blue-400">pahe</h1>
+      <h1 className="bg-blue-400">pahe</h1>
     </div>
   );
 }
