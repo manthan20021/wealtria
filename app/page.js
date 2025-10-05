1ba8ffa073b666f822f4d1e4fceed96d2a45b39e
Manthan harale <126886816+manthan20021@users.noreply.github.com>
2025-10-04 22:09:18 -0700

    add brack point for testing purpas

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1 className="bg-blue-400 text-pink-600">rugved umak..</h1>
+      <h1 className="sm:bg-blue-400 bg-amber-700 text-pink-600">rugved umak..</h1>
     </div>
   );
 }
